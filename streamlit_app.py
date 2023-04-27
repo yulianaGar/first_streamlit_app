0f13b72e5492aaff8ed018531570e5e5cdb91a45
YulianaGar <119898037+yulianaGar@users.noreply.github.com>
2023-04-27 12:40:05 -0600

    Update streamlit_app.py
    selector dinamico

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,4 +11,9 @@ streamlit.header('🍌🥭 Crea tu propio batido de frutas 🥝🍇')
 
 import pandas
 my_fruit_list=pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+# Pongamos una lista de selección aquí para que puedan escoger la fruta que quieren incluir 
+streamlit.multiselect("Recoger algunas frutas:", list(my_fruit_list.index)) 
+
+# Mostrar la tabla en la página.
+
 streamlit.dataframe(my_fruit_list)
