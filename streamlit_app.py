6b79e68cf90d1671ce2f5ae7f6cdb1622911e452
YulianaGar <119898037+yulianaGar@users.noreply.github.com>
2023-04-27 12:13:55 -0600

    Update streamlit_app.py
    iconos añadidos entre el texto

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,9 @@ import streamlit
 
 streamlit.title('Mi padres hacen comida saludable')
 streamlit.header('Menú de desayuno')
-streamlit.text('Omega 3 y avena con arándanos')
-streamlit.text('Batido de col rizada, espinacas y rúcula')
-streamlit.text('Huevo de gallinas camperas hervidas')
+streamlit.text('🥣 Omega 3 y avena con arándanos')
+streamlit.text('🥗Batido de col rizada, espinacas y rúcula')
+streamlit.text('🐔Huevo de gallinas camperas hervidas')
+streamlit.text('🥑🍞Tostada de aguacate')
+
+streamlit.header('🍌🥭 Crea tu propio batido de frutas 🥝🍇')
