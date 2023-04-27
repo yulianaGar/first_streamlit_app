20d86cd86a73a9fc88a799f6e46aaf283312f20e
YulianaGar <119898037+yulianaGar@users.noreply.github.com>
2023-04-27 12:37:19 -0600

    Update streamlit_app.py
    lista de un link con libreria de pandas

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,3 +8,7 @@ streamlit.text('🐔Huevo de gallinas camperas hervidas')
 streamlit.text('🥑🍞Tostada de aguacate')
 
 streamlit.header('🍌🥭 Crea tu propio batido de frutas 🥝🍇')
+
+import pandas
+my_fruit_list=pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
