9c7766f11e639ab3ec7bea8875be22bbac25e1f3
YulianaGar <119898037+yulianaGar@users.noreply.github.com>
2023-04-27 22:15:04 -0600

    Update streamlit_app.py
    otro cambio por error en la consulta

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,7 +44,7 @@ import snowflake.connector
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 
-my_cur.execute("select * from fruit_load_list")
+my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
 my_data_row = my_cur.fetchone()
 
 streamlit.text("the fruit tload list contains:")
