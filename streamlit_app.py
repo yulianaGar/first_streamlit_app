2641701659876e88870212c3a9de4eeaf7cfaff6
YulianaGar <119898037+yulianaGar@users.noreply.github.com>
2023-04-27 22:19:26 -0600

    Update streamlit_app.py
    modificaciones para mas en la lista

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,8 +45,8 @@ my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 
 my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
-my_data_row = my_cur.fetchone()
+my_data_rows=my_cur.fetchall()
 
-streamlit.text("the fruit tload list contains:")
-streamlit.text(my_data_row)
+streamlit.header("the fruit tload list contains:")
+streamlit.dataframe(my_data_rows)
 
